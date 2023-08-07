f0c598638985190258f89d27f788a8e23f3835ad
Rameet Mashta <rameet.mashta@codequotient.com>
2023-08-07 22:36:37 +0530

    test

--- git.sh
+++ git.sh
@@ -1,4 +1,4 @@
 git config --global gpg.format ssh
 git config --global user.signingkey ~/.ssh/id_ed25519.pub
-git config --global user.name "<username>"
-git config --global user.email "<email>"
+git config --global user.name "Rameet Mashta"
+git config --global user.email "rameet.mashta@codequotient.com"
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
 const firstFile = {};
+
+class A {};
